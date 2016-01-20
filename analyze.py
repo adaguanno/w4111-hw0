90e02fef2c99f189b17c78d239b6ecab448e3271
Ashley Daguanno <ashley.daguanno@gmail.com>
2016-01-20 14:42:46 -0500

    Updated program to treat the file as a csv file, rather than read each line as just a string.

--- analyze.py
+++ analyze.py
@@ -7,9 +7,24 @@ key = 'single malt scotch'
 
 with open('iowa-liquor-sample.csv') as f:
     for line in f:
+        attributes = line.split(",")
+        catName = attributes[12].lower()
+        descrip = attributes[16].lower() 
+        # descrip not 100% necessary -- check in case key is not in catName
+        if key in catName or key in descrip:
+           count = count + 1
+
+print (count)
+
+
+"""
+# alternate code that just treats each line as a string and
+# does a substring search
+with open('iowa-liquor-sample.csv') as f:
+    for line in f:
         cur = line.lower()
         if key in cur:
            count = count + 1
 
 print (count)
-
+"""
